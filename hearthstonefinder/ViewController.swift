4616eddec3d62c70a1b9a136aba07b845e0cdf0f
Steven Hawley <hawleyst@mail.gvsu.edu>
2015-11-18 16:31:48 -0500

    Basic UI

--- hearthstonefinder/ViewController.swift
+++ hearthstonefinder/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     //Array for classes
     var classes = Dictionary<String,Class>()
